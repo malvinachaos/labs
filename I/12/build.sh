e3f3784399e9681516017e9820bfb4e9155b7ff0
Marina Kalashnikova <malvinabluehair256@gmail.com>
2020-12-04 21:52:42 +0300

    12 is ready, but need to check again

--- I/12/build.sh
+++ I/12/build.sh
@@ -3,63 +3,55 @@
 # create random numbers:  echo $((1 + RANDOM % 10))
 # echo -ne
 file="a.txt"
-name=$(ls *.pas)
+name="three_arrays.pas"
 while [ -n "$1" ]
 do
     case $1 in
         "-h"|"--help")
             echo -e "building pascal program
-Usage build.sh [-h] [-n] [-c N [name]] [-b] [-v] [-r]
+Usage build.sh [-h|--help] [-c|--create] [-e|--edit] [-b|--build] [--run] [--show] [--remove]
 
 -h --help \t \t output this text
--c --create N [name] \t create file with random integers(N is range, like 10, 100, 1000, etc.)
--b --build \t \t compile to program, named 'program'
+-c --create \t \t create file with random integers
+-e --edit \t \t edit .txt files
+-b --build \t \t compile to program
 -v --verbose \t \t compilt with additional information
--r --remove \t \t remove all .txt files and 'program'
+-r --remove \t \t remove all .txt files and program
 "
-        shift
         ;;
 
        "-c"|"--create-test")
-            if [ -n "$3" ] 
-            then
-                file=$3
-            fi
-            for i in $(seq 1 50)
+            for i in "a.txt" "b.txt" "c.txt"
             do
-                if [ $((1 + RANDOM % 8)) -eq 5 ]
-                then
-                    for j in $(seq 1 50)
-                    do
-                        if [ $((1 + RANDOM % 5)) -eq 5 ]
-                        then
-                            echo -ne "$((1+ RANDOM % $2)) " >> $file
-                        else echo -ne "$j " >> $file
-                        fi
-                    done
-                else 
-                    for j in $(seq 1 50)
-                    do 
-                        echo -ne "$j " >> $file
-                    done
-                fi
-                echo >> $file
+                for j in $(seq 1 100)
+                do
+                    echo -ne "$((1 + RANDOM % 100)) " >> $i
+                done
+                echo >> $i
             done
-        shift
+            echo "Файлы созданы"
+        ;;
+
+        "-e"|"--edit")
+            vim -o3 a.txt b.txt c.txt
         ;;
 
         "-b"|"--build")
-            fpc $name -oprogram && rm -f *.o
-        shift
+            fpc $name -othree_arrays.exe && rm -f *.o
         ;;
 
-        "-v"|"--verbose")
-            fpc -ved $name -oprogram && rm -f *.o
-        shift
+        "--run")
+            echo -ne "\n\nЗапуск программы...\n\n"
+            ./three_arrays.exe a.txt b.txt c.txt out.txt
         ;;
 
-        "-r"|"--remove")
-            rm -fv program *.txt
+        "--show")
+            echo -ne "\n\n-~~====[Содержимое файла out.txt]====~~-\n"
+            cat out.txt
+        ;;
+
+        "--remove")
+            rm -fv *.exe *.txt
         ;;
     esac
 shift
--- I/12/three_arrays.pas
+++ I/12/three_arrays.pas
@@ -2,6 +2,123 @@ PROGRAM Made_In_Heaven;
 
 USES sysutils;
 
-BEGIN
+TYPE onedim = array of integer;
 
+PROCEDURE out_choice(i: byte);
+Begin
+
+End;
+
+PROCEDURE finarr(var f: text; var x: onedim; n: byte);
+Var i: byte;
+Begin
+    reset(f);
+    for i:= 0 to n do
+        read(f, x[i]);
+    close(f);
+End;
+
+PROCEDURE fouarr(var f: text; var x: onedim; n: byte);
+Var i: byte;
+Begin
+    for i:= 0 to n do
+        write(f, x[i], ' ');
+    writeln(f);
+End;
+
+FUNCTION minimum(x: onedim; n: byte): integer;
+Var i: byte;
+    min: integer;
+Begin
+    min:= x[0];
+    for i:= 1 to n do
+        if x[i] < min then
+            min:= x[i];
+    minimum:= min;
+End;
+
+VAR arr: onedim;
+    c: array [1..3] of integer;
+    itxt, otxt: text;
+    a, b, mini, number: integer;
+    n, i: byte;
+    fexi, aexi: boolean;
+
+BEGIN
+    aexi:= paramcount = 4;
+    fexi:= fileexists(argv[1]) and fileexists(argv[2]) and fileexists(argv[3]);
+
+    if aexi and fexi then
+    begin
+        assign(otxt, argv[4]);
+        rewrite(otxt);
+
+        write('Задайте концы отрезка AB: ');
+        readln(a, b);
+        if a > b then
+        begin 
+            writeln('Перестановка');
+            a += b;
+            b:= a - b;
+            a -= b;
+        end;
+         
+        for i:= 1 to 3 do
+        begin
+            assign(itxt, argv[i]);
+        
+            repeat
+                write('Введите размерность массива ');
+                case i of
+                    1: write('a: ');
+                    2: write('b: ');
+                    3: write('c: ');
+                end;
+                readln(n);
+            until (n >= 2) and (n <= 100);
+            setlength(arr, n);
+            n -= 1;
+   
+            finarr(itxt, arr, n);
+
+            write(otxt, 'Значения массива ');
+            case i of
+                1: writeln(otxt, 'a:');
+                2: writeln(otxt, 'b:');
+                3: writeln(otxt, 'c:');
+            end;
+
+            fouarr(otxt, arr, n);
+
+            c[i]:= minimum(arr, n); 
+            writeln(otxt, 'Минимальное число этого массива: ', c[i], #13#10, #13#10);
+        end;
+
+        mini:= b+1;
+        number:= 0;
+        for i:= 1 to 3 do
+            if (mini > c[i]) and (c[i] >= a) and (c[i] <= b) then
+            begin 
+                mini:= c[i];
+                number:= i;
+            end;
+
+        if number = 0 then 
+             writeln(otxt, 'Ни одно найденное значение не принадлежит отрезку')
+        else 
+        begin
+            write(otxt, 'Минимальное значение, принадлежащее [', 
+                  a, '; ', b, ']', ' имеет массив ');
+            case number of
+                1: writeln(otxt, 'a');
+                2: writeln(otxt, 'b');
+                3: writeln(otxt, 'c');
+            end;
+        end;
+        close(otxt);
+    end
+    else
+    begin
+        writeln('Использовани:', #13#10, './three_arrays.exe a.txt b.txt c.txt out.txt');
+    end;
 END.
