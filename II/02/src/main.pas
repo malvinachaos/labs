016926cf9b121f62cdd19e32f559373550dede84
Marina Kalashnikova <malvinabluehair256@gmail.com>
2021-03-04 23:31:56 +0000

    Need to fix the problem with deleting string

--- II/02/src/main.pas
+++ II/02/src/main.pas
@@ -2,9 +2,11 @@ PROGRAM H;
 
 USES io, udef;
 
-VAR run: boolean = false;
+VAR run: boolean = true;
     menu: byte = 0;
-    n: byte;
+    m, n, len, k, l: byte;
+    istr, istr_buff, ostr: ustr;
+
 
 BEGIN
 
@@ -20,7 +22,6 @@ begin
     readln(menu);
 
     case menu of
-    begin
         1:
         begin
             assign(log, logname);
@@ -37,6 +38,54 @@ begin
             write('Введите число(от 0 до 255): ');
             readln(n);
 
+            if aex and fex then
+            begin
+                assign(itxt, paramstr(1));
+                stread(log, istr, itxt);
+            end
+            else if (not aex) or (not fex) then
+            begin
+                if not aex then
+                    WRITELN(log,
+                        '[MAIN] WARNING: User did not specify files on command line',
+                        #13#10);
+
+                if not fex then
+                    WRITELN(log,
+                        '[MAIN] ERROR: User specify file that does not exist',
+                        #13#10);
+            end
+            else stread(log, istr);
+
+            WRITELN(log, '[MAIN]: String was read');
+
+            m:= getlength(log, istr);
+            while m > 0 do
+            begin
+                WRITELN(log);
+                k:= find(log, ' ', istr);
+                stropy(log, istr, 1, k-1, istr_buff);
+                if getlength(log, istr_buff) <= n then
+                    con(log, istr_buff, ostr, ostr);
+                remove(log, istr, 1, k);
+                WRITE(log, '    [MAIN -> while ... do -> STRIDE]: ');
+                stride(log, istr);
+                WRITELN(log, '  [MAIN -> while ... do]: Size of source string is ', m);
+            end;
+
+            if aex and fex then
+            begin
+                stride(log, ostr, otxt);
+                WRITELN(log, '[MAIN]: New string was written to output file');
+
+                close(otxt);
+                WRITELN(log, '[MAIN]: Output file was closed');
+            end
+            else
+            begin
+                stride(log, ostr);
+                WRITELN(log, '[MAIN]: New string was written to console');
+            end;
 
             close(log);
         end;
