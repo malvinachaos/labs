1626ddf310ca0b27329d2a9c339afb217748ee10
Marina Kalashnikova <malvinabluehair256@gmail.com>
2021-03-13 14:40:47 +0300

    Problem with methods

--- II/03/src/main.pas
+++ II/03/src/main.pas
@@ -3,7 +3,9 @@ PROGRAM paste_n_taste_your_pain;
 USES types, methods, funcs, io;
 
 VAR a, b, e, x: real;
+    eps: byte = 0;
     res1, res2, res3: real;
+    it1, it2, it3: byte;
     choice: byte = 0;
     formula_1: string = '        ⁷√x * sin²x' + #13#10 + 
                         '⁵√e⁻ˣ + ──────────── - x' + #13#10 +
@@ -34,7 +36,8 @@ case menu of
 	begin
 		assign(log, logname);
 		rewrite(log);
-		WRITELN(log, '_.=[PROGRAM START]=._');
+		WRITELN(log, '_.=[PROGRAM START]=._', #13#10,
+                '[MAIN]: Logfile is reset', #13#10);
 		close(log);
 	end;
 
@@ -45,27 +48,34 @@ case menu of
  
         writeln('1)', #13#10, formula_1, #13#10);
         writeln(#13#10, '2)', #13#10, formula_2, #13#10);
-        segments(log, e, choice);
+        segments(log, a, b, e, choice);
+        eps:= trunc(ln(round(1/e))/ln(10));
+        WRITELN(log, '[MAIN]: Count of output symbols after dot is ', eps, #13#10);
  
         case choice of
             1:
             begin
-                res1:= simple_iterations(log, e, f_12);
-                res2:= tangent(log, e, f_12_x);
-                res3:= newton(log, e, f_12_x);
+                simple_iterations(log, eps, a, b, e, f_12, res1, it1);
+                tangent(log, eps, a, b, e, f_12_x, res2, it2);
+                newton(log, eps, e, f_12_x, res3, it3);
             end;
  
             2:
             begin
-                res1:= simple_iterations(log, e, f_20);
-                res2:= tangent(log, e, f_20_x);
-                res3:= newton(log, e, f_20_x);
+                simple_iterations(log, eps, a, b, e, f_20, res1, it1);
+                tangent(log, eps, a, b, e, f_20_x, res2, it2);
+                newton(log, eps, e, f_20_x, res3, it3);
             end;
         end;
  
-        writeln(log, '[MAIN]: Result of simple iterations method: ', res1:8:5);
-        writeln(log, '[MAIN]: Result of tangent method: ', res2:8:5);
-        writeln(log, '[MAIN]: Result of Newton method: ', res3:8:5);
+        writeln('Результат метода простых итераций = ', res1:8:eps, #13#10,
+                'Количество итераций = ', it1, #13#10);
+
+        writeln('Результат метода секущих: ', res2:8:eps, #13#10,
+                'Количество итераций = ', it2, #13#10);
+
+        writeln('Результат метода Ньютона = ', res3:8:eps, #13#10,
+                'Количество итераций = ', it3, #13#10);
  
         close(log);
 	end;
--- II/03/src/methods.pas
+++ II/03/src/methods.pas
@@ -4,46 +4,43 @@ INTERFACE
 
     USES types;
 
-    FUNCTION simple_iterations(var log: text; const e: real; f: func): real;
-    FUNCTION tangent(var log: text; const e: real; f: func): real;
-    FUNCTION newton(var log: text; const e: real; f: func): real;
-    PROCEDURE segments(var log: text; var e: real; var ch: byte);
+    PROCEDURE simple_iterations(var log: text; const eps: byte; const a, b, e: real; f: func; var x1: real; var i: byte);
+    PROCEDURE tangent(var log: text; const eps: byte; const a, b, e: real; f: func; var x1: real; var i: byte);
+    PROCEDURE newton(var log: text; const eps: byte; const e: real; f: func; var x1: real; var i: byte);
+    PROCEDURE segments(var log: text; var a, b, e: real; var ch: byte);
 
 
 IMPLEMENTATION
 
-    FUNCTION simple_iterations(var log: text; const e: real; f: func): real;
-    Var x0, x1, a, b: real;
-        i: byte = 0;
+    PROCEDURE simple_iterations(var log: text; const eps: byte; const a, b, e: real; f: func; var x1: real; var i: byte);
+    Var x0: real;
     Begin
-        repeat
-            write('Введите концы отрезка [a,b]: ');
-            readln(a, b);
-        until (a < b);
+        x1:= 0;
+        i:= 0;
+
         WRITELN(log, '[SIMPLE_ITERATIONS]: User set segments ends: [',a, ', ', b, ']');
         x0:= (a + b)/2;
         WRITELN(log, '[SIMPLE_ITERATIONS]: First approach = ', x0);
 
         repeat
             x1:= f(x0);
-            x0:= x1;
             i += 1;
-            WRITELN(log, '  [SIMPLE_ITERATIONS]: ', i:3, ': f(', x0:5:3, ') = ', x1);
+            WRITELN(log, '  [SIMPLE_ITERATIONS]: _[', i:3, ']_ | f(', x0:3:eps, ') = ', x1:3:eps);
+            x0:= x1;
         until (abs(x1 - x0) < e) or (a < x1) and (x1 < b) or (i = 100);
 
-        result:= x1;
-        WRITELN(log);
+        WRITELN(log, '[SIMPLE_ITERATIONS]:', #13#10, '    Count of iterations = ', i, 
+                #13#10, '    Root is = ', x1, #13#10);
     End;
 
-    FUNCTION tangent(var log: text; const e: real; f: func): real;
-    var x0, x1, tmp: real;
+    PROCEDURE tangent(var log: text; const eps: byte; const a, b, e: real; f: func; var x1: real; var i: byte);
+    var x0, tmp: real;
     begin
-        write('Введите первое начальное приближение: ');
-        readln(x0);
-        WRITELN(log, '[TANGENT]: First approaching is ', x0);
+        x0:= a;
+        x1:= b;
+        i:= 0;
 
-        write('Второе начальное приближение: ');
-        readln(x1);
+        WRITELN(log, '[TANGENT]: First approaching is ', x0);
         WRITELN(log, '[TANGENT]: Second approaching is ', x1);
 
         while abs(x1 - x0) > e do
@@ -51,32 +48,38 @@ IMPLEMENTATION
             tmp:= x1 - (f(x1)*(x1 - x0)) / (f(x1) - f(x0));
             x0:= x1;
             x1:= tmp;
-            WRITELN(log, '  [TANGENT]: | ', x1, ' - ', x0, ' | > ', e);
+            i += 1;
+            WRITELN(log, '  [TANGENT]: _[', i:3, ']_ | ', x1, ' - ', x0, ' | > ', e);
         end;
 
-        result:= x0;
-        WRITELN(log);
+        WRITELN(log, '[TANGENT]:', #13#10, '    Count of iterations = ', i,
+                #13#10, '    Root is = ', x1, #13#10);
     end;
 
-    FUNCTION newton(var log: text; const e: real; f: func): real;
-    var x0, x1, tmp: real;
+    PROCEDURE newton(var log: text; const eps: byte; const e: real; f: func; var x1: real; var i: byte);
+    var x0, tmp: real;
     begin
-        write('Введите начальное приближение: ');
+        x1:= 0;
+        i:= 0;
+
+        write('[Метод Ньютона]: Введите начальное приближение: ');
         readln(x0);
+        writeln();
         WRITELN(log, '[NEWTON]: Approaching is ', x0);
 
         WRITELN(log, '  [NEWTON]: |x1-x0| < e');
         repeat
             tmp:= x0;
             x1:= x0 - f(x0) / ( (f(x0+e/2) - f(x0-e/2)) /e);
-            WRITELN(log, '  [NEWTON]: | ', x1, ' - ', x0, ' | < ', e);
+            i += 1;
+            WRITELN(log, '  [NEWTON]: _[', i:3, ']_ | ', x1, ' - ', x0, ' | < ', e);
         until (abs(x1 - x0) >= e);
 
-        result:= x1;
-        WRITELN(log);
+        WRITELN(log, '[NEWTON]:', #13#10, '    Count of iterations = ', i,
+                #13#10, '    Root is = ', x1, #13#10);
     end;
 
-    PROCEDURE segments(var log: text; var e: real; var ch: byte);
+    PROCEDURE segments(var log: text; var a, b, e: real; var ch: byte);
     Begin
         repeat
             write(': ');
@@ -85,11 +88,16 @@ IMPLEMENTATION
         WRITELN(log, '[SEGMENTS]: User choose a function number = ', ch);
 
         repeat
+            write(': Введите концы отрезка [a,b]: ');
+            readln(a, b);
+            writeln();
+        until (a < b);
+
+        repeat
             write('Введите точность ε: ');
             readln(e);
         until (e > 0) and (e < 1);
-        WRITELN(log, '[SEGMENTS]: User specified accuracy: ', e);
-        WRITELN(log);
+        WRITELN(log, '[SEGMENTS]: User specified accuracy: ', e, #13#10);
     End;
 
 END.
